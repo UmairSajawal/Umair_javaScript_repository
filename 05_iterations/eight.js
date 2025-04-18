8e9e78cfadb463d1e7cc20cc09114f53cdaa52ea
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2025-04-18 16:02:55 +0500

    Use of reduce() method

---
+++ 05_iterations/eight.js
@@ -0,0 +1,35 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+//with arrow function:
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+// Example: find the total price of shopping cart using reduce method
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
