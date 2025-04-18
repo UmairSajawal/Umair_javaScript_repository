29253df13f889bc2e53802f8e33b72e9ff3ca0f6
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2025-04-18 16:01:47 +0500

    Use of map() method

---
+++ 05_iterations/seven.js
@@ -0,0 +1,14 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})   //Note: when scope {} use or open, it is compulsory to use return keyword
+
+
+
+//called chaining process or method:
+//example:  const newNum = myNumers.map().map().filter()...etc
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
