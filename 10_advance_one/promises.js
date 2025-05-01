8004c4b41c5fbf3530b2db85d307324dde8d0fb0
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2025-05-01 13:12:55 +0500

    promises.js

--- 10_advance_one/promises.js
+++ 10_advance_one/promises.js
@@ -86,3 +86,28 @@ async function consumePromiseFive(){
     }
 }
 consumePromiseFive();
+
+async function getAllUsers(){
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        console.log(data);
+    } catch(error){
+        console.log("E:", error)
+    }
+}
+getAllUsers();
+
+
+// Important Note: fetch() always execute firstly, then executes other tasks like setTimeout(), ....,etc.
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error));
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
